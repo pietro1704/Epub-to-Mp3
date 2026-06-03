44e6711b9c54c37b6b356b7f6424e86b4a806ff3
Pietro Pugliesi <pietropugliesi@icloud.com>
2026-06-03 18:10:44 -0300

    test(flutter): remove stale sync engine import

--- flutter_app/test/sync_engine_rebind_integration_test.dart
+++ flutter_app/test/sync_engine_rebind_integration_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:flutter_app/services/sync_engine.dart';
 import 'package:flutter_app/state/providers.dart';
 
 void main() {
